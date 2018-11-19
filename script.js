291b46d1ee3a31123097b45b9e7f2de0b26d22a2
Peter <korotushak@gmail.com>
2018-11-19 19:06:13 +0200

    some correct

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 'use strict';
 
 let money = prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
-    expense = prompt('Введите обязательную статью расходов в этом месяце'),
-    expenseCost = prompt('Во сколько обойдется?');
+    time = prompt('Введите дату в формате YYYY-MM-DD');
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        expense: expenseCost
-    },
+    expenses: {},
     optionalExpenses: {},
     optionalExpenses: '',
     income: [],
     savings: false
 };
 
-alert((appData.budget - appData.expenses.expense)/30)
+let expense = prompt('Введите обязательную статью расходов в этом месяце'),
+    expenseCost = prompt('Во сколько обойдется?');
+
+appData.expenses.expense=expenseCost;
+
+alert((appData.budget/30));
